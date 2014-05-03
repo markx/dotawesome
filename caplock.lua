852c195a40d3a1b5110fe63da69452d40a0d1404
markwithk <markxu18@gmail.com>
2014-05-03 14:08:29 -0400

    caplock add notification

--- caplock.lua
+++ caplock.lua
@@ -1,5 +1,9 @@
 local wibox=require("wibox")
 local awful=require("awful")
+local naughty=require("naughty")
+
+local caplock_state = nil
+local nid = nil
 
 function caplock_update ()
     local fd = io.popen("xset -q")
@@ -24,9 +28,12 @@ function caplock_toggle ()
 	--the last state is "ON", so now it should be "OFF"
 	caplock_state='0'
 	text = "|a|" 
+	nid=naughty.notify({text = "Caplock Disabled",position="bottom_right",bg='#3D3D3D', fg='#ffffff',width = 130,height=50,timeout=3,replaces_id=nid}).id
+
     elseif caplock_state == '0' then
 	caplock_state='1'
 	text = "|A|" 
+	nid=naughty.notify({text = "Caplock Enabled",position="bottom_right",bg='#3D3D3D',fg='#00FF00', width = 120,height=50,timeout=3,replaces_id=nid}).id
     else
 	text= 'unknown'
     end
